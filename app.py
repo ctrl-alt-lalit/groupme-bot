9c572586e4fe9adbd18edfeae8cb216ec8141f7f
lbauskar <labauskar@gmail.com>
2019-05-17 07:17:13 -0500

    changed to OO style so bots can be duplicated

--- app.py
+++ app.py
@@ -5,35 +5,45 @@ from urllib.request import Request, urlopen
 from flask import Flask, request
 
 app = Flask(__name__)
+
+
+class GMBot:  # parent class for all GroupMe bots
+    def __init__(self, bot_id, bot_name, activate=True):
+        self.id = bot_id
+        self.name = bot_name
+        self.active = activate
+
+    @staticmethod
+    def send_message(msg):
+        url = "https://api.groupme.com/v3/bots/post"
+
+        data = {
+            "bot_id": os.getenv("GM_BOT_ID"),
+            "text": msg
+        }
+
+        request_msg = Request(url, urlencode(data).encode())
+        urlopen(request_msg).read().decode()
+
+    def parse(self, data):  # meant to be overwritten by children
+        if ("@" + self.name in data["text"]) and (data["name"] != self.name):
+            msg = "hi @{}".format(data["name"])
+            self.send_message(msg)
+
+
+# child classes
+class LFBot(GMBot):
+    def parse(self, data):
+        if ("@" + self.name in data["text"]) and (data["name"] != self.name):
+            msg = "hi @{}, you said {}".format(data["name"], data["text"])
+            self.send_message(msg)
+
+
+lf_bot = LFBot(os.getenv("LF_BOT_ID"), os.getenv("LF_BOT_NAME"))
+
+
 @app.route('/', methods=["POST"])
 def read_group():
     data = request.get_json()
-
-    if ("@"+os.getenv("GM_BOT_NAME") in data["text"]) and (data["name"] != os.getenv("GM_BOT_NAME")):
-        msg = "hi @{}".format(data["name"])
-        send_message(msg)
-
+    lf_bot.parse(data)
     return "done", 200
-
-
-def send_message(msg):
-    url = "https://api.groupme.com/v3/bots/post"
-
-    data = {
-        "bot_id": os.getenv("GM_BOT_ID"),
-        "text": msg
-    }
-
-    request_msg = Request(url, urlencode(data).encode())
-    urlopen(request_msg).read().decode()
-
-
-def echo(data):
-    # only reply to other people
-    if data["name"] != os.getenv("GM_BOT_NAME"):
-        msg = '{} sent "{}"'.format(data["name"], data["text"])
-        send_message(msg)
-
-    return "done", 200
-
-
