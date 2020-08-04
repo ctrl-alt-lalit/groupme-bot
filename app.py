194892a6ff2d5e148b0af0b119089e844227055f
lbauskar <labauskar@gmail.com>
2020-08-04 18:15:20 -0500

    Added bot for RA chat

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ from flask import Flask, request
 from gmbot import GroupMeBot
 from lfbot import LFBot
 from sabot import SABot
+from rabot import RABot
 
 
 app = Flask(__name__)
 
 
-test_bot = LFBot(getenv("TEST_BOT_ID"), getenv("TEST_BOT_NAME"), getenv("TEST_GROUP_ID"))
+test_bot = RABot(getenv("TEST_BOT_ID"), getenv("TEST_BOT_NAME"), getenv("TEST_GROUP_ID"))
 @app.route("/test", methods=["POST"])
 def read_test_group():
     data = request.get_json()
@@ -29,4 +30,12 @@ sa_bot = SABot(getenv("SA_BOT_ID"), getenv("SA_BOT_NAME"), getenv("SA_GROUP_ID")
 def read_sa_group():
     data = request.get_json()
     sa_bot.chat(data)
+    return "OK", 200
+
+
+ra_bot = RABot(getenv("RA_BOT_ID"), getenv("RA_BOT_NAME"), getenv("RA_GROUP_ID"))
+@app.route("/ra", methods=["POST"])
+def read_ra_group():
+    data = request.get_json()
+    ra_bot.chat(data)
     return "OK", 200
---
+++ rabot.py
@@ -0,0 +1,16 @@
+from gmbot import GroupMeBot
+
+class RABot(GroupMeBot):
+    """Meant for the RA chat"""
+    def chat(self, data):
+        if data["name"] != self.name and data["name"] != "GroupMe":
+            text = data["text"].lower()
+            if "@everyone" in text:
+                self.at_everyone()
+            if "!!!test" in text:
+                self.send_message("posts")
+
+    def at_everyone(self):
+        """Mention every member of a group."""
+        for mention in self.create_multi_mention(self.get_member_list(), bold_location=(0, 3)):
+            self.send_message(msg="sup", attachments=[mention])
